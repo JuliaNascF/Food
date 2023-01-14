cb831b7f508aa1e41a9806164b35c93912d8f7ba
JuliaNascF <jubilete.fazenda12@gmail.com>
2023-01-14 01:13:30 -0300

    versao completa

--- functions.js
+++ functions.js
@@ -1,3 +1,12 @@
+var nomePrato, precoPrato, precoBebida, nomeBebida, nomeSobremesa, precoSobremesa;
+
+
+var precoTotal
+
+
+
+
+
 function selecionarPrato(elementoselecionado){
     const elemento = document.querySelector(" div.pratos .selecionado");
       if( elemento !== null){
@@ -11,7 +20,8 @@ function selecionarPrato(elementoselecionado){
         elementoselecionado.classList.toggle("selecionado");
         const check = elementoselecionado.querySelector(" div.pratos div.selecionado .icon")
         check.classList.toggle("display-none")
-        
+         nomePrato = document.querySelector(".selecionado .nome-prato p1").innerHTML;
+         precoPrato = document.querySelector(".selecionado .preco-prato p3").innerHTML;
 
     }
 
@@ -27,7 +37,9 @@ function selecionarPrato(elementoselecionado){
             elementoselecionado.classList.toggle("selecionado");
             const check = elementoselecionado.querySelector(" div.bebidas div.selecionado .icon")
             check.classList.remove("display-none")
-            
+            nomeBebida = document.querySelector(".selecionado .nome-bebida p1").innerHTML;
+            precoBebida= document.querySelector(".selecionado .preco-bebida p3").innerHTML;
+
     
         }
     
@@ -44,6 +56,9 @@ function selecionarPrato(elementoselecionado){
                  elementoselecionado.classList.toggle("selecionado");
                 const check = elementoselecionado.querySelector(".icon")
                 check.classList.remove("display-none")
+                 nomeSobremesa = document.querySelector(".selecionado .nome-sobremesa p1").innerHTML;
+                 precoSobremesa= document.querySelector(".selecionado .preco-sobremesa p3").innerHTML;
+
                 
         
             }
@@ -57,8 +72,61 @@ function selecionarPrato(elementoselecionado){
                bloqueado.classList.add("display-none");
                desbloqueado.classList.remove("display-none");
                desbloqueado.disabled = false ;
-               console.log("juju")
             }
 
            }
 
+            function fecharPedido(){
+              const fundo = document.querySelector(".confirmacao-fundo")
+              const verde = document.querySelector(".confirma")
+              fundo.classList.remove("display-none")
+              verde.classList.remove("display-none")
+
+              
+              
+              document.querySelector(" div.confirma div.confirmado div.pedido .nome-prato ").innerHTML= nomePrato; 
+              document.querySelector(" div.confirma div.confirmado div.pedido .nome-bebida ").innerHTML= nomeBebida; 
+              document.querySelector(" div.confirma div.confirmado div.pedido .nome-sobremesa ").innerHTML= nomeSobremesa;
+               
+              precoPratoReal = precoPrato.replace( "R$ ", "");
+              precoBebidaReal = precoBebida.replace("R$ " , "");
+              precoSobremesaReal = precoSobremesa.replace("R$ " , "" );
+              
+              document.querySelector(" div.confirma div.confirmado div.pedido .preco-prato ").innerHTML= precoPratoReal; 
+              document.querySelector(" div.confirma div.confirmado div.pedido .preco-bebida ").innerHTML= precoBebidaReal; 
+              document.querySelector(" div.confirma div.confirmado div.pedido .preco-sobremesa ").innerHTML= precoSobremesaReal;
+
+              precoPratoConvert = Number(precoPratoReal.replace("," , "."));
+              precoBebidaConvert = Number(precoBebidaReal.replace("," , "."));
+              precoSobremesaConvert = Number(precoSobremesaReal.replace("," , "."));
+              
+              var tudo = precoPratoConvert + precoBebidaConvert + precoSobremesaConvert; 
+              precoTotal = "R$ "+tudo.toFixed(2);
+                precoTotal= precoTotal.replace(".",",");
+               
+                document.querySelector(" div.confirma div.confirmado div.total-preco  .PRECO ").innerHTML= precoTotal;
+                precoTotal= precoTotal.replace(",",".");
+                console.log(precoTotal);
+              
+            }
+
+            function tudoCerto() {
+              var nomePessoa = prompt("Me fala seu nome ai:");
+              if (nomePessoa === null || nomePessoa === "") {
+                  nomePessoa = "Nome não informado.";
+              }
+              var enderecoPessoa = prompt(" Agora seu endereço:");
+              if (enderecoPessoa === null || enderecoPessoa === "") {
+                  enderecoPessoa = "Endereço não informado.";
+              }
+              const mensagem = `Olá, gostaria de fazer esse pedido:\n- Prato: ${nomePrato}\n- Bebida: ${nomeBebida}\n- Sobremesa: ${nomeSobremesa}\n- Total: ${precoTotal}\n- Nome: ${nomePessoa}\n- Endereço: ${enderecoPessoa}`;
+              window.open(`https://wa.me/5521985052292?text=${encodeURIComponent(mensagem)}`);
+
+          }
+            
+            function Cancel(){
+              const sumirfundo = document.querySelector("div.confirmacao-fundo ");
+              const sumirverde = document.querySelector("div.confirma")
+              sumirfundo.classList.add("display-none")
+              sumirverde.classList.add("display-none")
+            }
